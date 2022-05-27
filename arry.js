0e8b8b7508f3746a53720c2e111938f31a989c5e
amannitp <amannitp272@gmail.com>
2022-05-27 15:17:10 +0530

    first commit

---
+++ arry.js
@@ -0,0 +1,25 @@
+// Array
+
+//let a=[] // array declaration
+let a=[1,2,3,4,5,'aman',true]
+
+console.log(a)
+
+// array predefinrd method
+// push method
+a.push(100)
+console.log(a)
+
+// pop method
+a.pop()
+a.pop()
+
+console.log(a)
+
+// unshift method
+a.unshift("anchal")
+console.log(a)
+
+// shift 
+a.shift()
+console.log(a)
---
+++ varible.js
@@ -0,0 +1,59 @@
+/*
+var x=10;
+console.log(x)
+
+var x='aman';
+console.log(x)
+*/
+
+// var keyword problem is that it can be redeclared
+
+// var can reassigned
+/*
+var b=true
+var x =1000
+
+let name='pepcoding'
+
+console.log(name)
+*/
+
+/*
+ if(true){
+    var num=10;
+     console.log(num) //output 10
+ }
+
+ console.log(num) // output 10
+ */
+
+ 
+ /*
+if(true){
+    let num=20
+    console.log(20) //output 20
+}
+console.log(num) // output error
+*/
+
+/*
+function hello(){
+    var num=20;
+    console.log(num) // output 20
+}
+hello()
+console.log(num)  // error
+*/
+
+/*
+function hello(){
+    let num=20
+    console.log(num) // output 20
+}
+hello()
+console.log(num)  // output error
+*/
+
+const num=20
+
+
